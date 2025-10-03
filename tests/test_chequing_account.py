5e039f543098ceadf422a2fa4f6ad002eb1ee141
Lichao Huang <lhuang38@rrc.ca>
2025-10-03 10:44:15 -0500

    chequing account test init

---
+++ tests/test_chequing_account.py
@@ -0,0 +1,18 @@
+
+from bank_account.bank_account import BankAccount
+import unittest
+from bank_account.chequing_account import ChequingAccount
+from datetime import date
+
+
+class TestChequingAccount(unittest.TestCase):
+
+    def test_init_sets_attributes_with_valid_inputs(self):
+        account = ChequingAccount(123,321,-50, date(2000,10,10), -10.0, 0.1)
+
+        self.assertEqual(123, account._BankAccount__account_number)
+        self.assertEqual(321, account._BankAccount__client_number)
+        self.assertEqual(-50, account._BankAccount__balance)
+        self.assertEqual(date(2000,10,10), account._date_created)
+        self.assertEqual(-10, account._ChequingAccount__overdraft_limit)
+        self.assertEqual(0.1, account._ChequingAccount__overdraft_rate)
